4d85af1f2b2019184268e7fea56839c3621fa8a5
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-12-12 23:12:53 +0200

    task not finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { getPhotos } from 'Services/Services';
@@ -6,83 +6,78 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 
-export class App extends React.Component {
-  state = {
-    imagesData: [],
-    userInput: 'sakura',
-    page: 1,
-    loader: false,
-    isOpenedModal: false,
-    modalImgUrl: '',
-  };
-  async componentDidMount() {
-    try {
-      this.setState({ loader: true });
-      const images = await getPhotos(this.state.userInput, this.state.page);
-      console.log(images);
-      this.setState({ imagesData: [...images.hits] });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      this.setState({ loader: false });
-    }
-  }
-  async componentDidUpdate(_, prevState) {
-    if (
-      this.state.userInput !== prevState.userInput ||
-      prevState.page !== this.state.page
-    ) {
+export const App = () => {
+  const [imagesData, setImagesData] = useState([]);
+  const [userInput, setUserInput] = useState('sakura');
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(false);
+  const [isOpenedModal, setIsOpenedModal] = useState(false);
+  const [modalImgUrl, setModalImgUrl] = useState('');
+
+  useEffect(() => {
+    async function getImages() {
       try {
-        this.setState({ loader: true });
-        const images = await getPhotos(this.state.userInput, this.state.page);
-        console.log(images);
-        this.setState(prevState => ({
-          imagesData: [...prevState.imagesData, ...images.hits],
-        }));
+        setLoader(true);
+        const images = await getPhotos(userInput, page);
+        setImagesData(images.hits);
       } catch (error) {
         console.log(error);
       } finally {
-        this.setState({ loader: false });
+        setLoader(false);
       }
     }
-  }
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+    getImages();
+  }, []);
+
+  useEffect(() => {
+    async function addImmages() {
+      // if (userInput !== prevState.userInput || prevState.page !== page) {
+      try {
+        setLoader(true);
+        const images = await getPhotos(userInput, page);
+        if (page === 1) {
+          setImagesData(images.hits);
+        } else {
+          setImagesData(prevState => [...prevState, ...images.hits]);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoader(false);
+      }
+    }
+    // }
+    addImmages();
+  }, [userInput, page]);
+
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
   };
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
+    setPage(1);
+    setLoader(true);
     const inputResult = event.currentTarget.elements.input.value;
-    this.setState({ userInput: inputResult, page: 1, imagesData: [] });
+    setImagesData(inputResult);
   };
-  openModal = imgUrl => {
-    this.setState(prevState => ({
-      isOpenedModal: !prevState.isOpenedModal,
-      modalImgUrl: imgUrl,
-    }));
-  };
-  closeModal = () => {
-    this.setState(prevState => ({ isOpenedModal: !prevState.isOpenedModal }));
+  const openModal = imgUrl => {
+    setIsOpenedModal(prev => !prev);
+    setModalImgUrl(imgUrl);
   };
 
-  render() {
-    return (
-      <div>
-        <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery
-          imagesData={this.state.imagesData}
-          openModal={this.openModal}
-        />
-        {this.state.imagesData.length !== 0 ? (
-          <Button updatePage={this.handleLoadMore} />
-        ) : null}
-        {this.state.loader === true ? <Loader /> : null}
-        {this.state.isOpenedModal && (
-          <Modal
-            closeModal={this.closeModal}
-            modalImgUrl={this.state.modalImgUrl}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  const closeModal = () => {
+    setIsOpenedModal(prev => !prev);
+  };
+
+  return (
+    <div>
+      <Searchbar onSubmit={onSubmit} />
+      <ImageGallery imagesData={imagesData} openModal={openModal} />
+      {imagesData.length !== 0 ? <Button updatePage={handleLoadMore} /> : null}
+      {loader === true ? <Loader /> : null}
+      {isOpenedModal && (
+        <Modal closeModal={closeModal} modalImgUrl={modalImgUrl} />
+      )}
+    </div>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
 import s from './Modal.module.css';
 
-export class Modal extends React.Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ modalImgUrl, closeModal }) => {
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
 
-  handleKeyDown = event => {
-    if (event.key === 'Escape') {
-      this.props.closeModal();
-    }
-  };
-
-  render() {
-    const { modalImgUrl, closeModal } = this.props;
-    return (
-      <div className={s.overlay} onClick={closeModal}>
-        <img src={modalImgUrl} alt="modal img" className={s.modal} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={s.overlay} onClick={closeModal}>
+      <img src={modalImgUrl} alt="modal img" className={s.modal} />
+    </div>
+  );
+};
